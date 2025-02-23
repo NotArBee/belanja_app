d739633812a62020a56a1a76f2676d8f65095eab
Ardev <13120210004@umi.ac.id>
2025-02-24 00:11:22 +0700

    feat : login dan signup screen telah ditambahkan

---
+++ lib/features/authentication/screen/signup/signup.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:t_store/common/styles/spacing_styles_signup_screen.dart';
+import 'package:t_store/features/authentication/screen/signup/widgets/signup_form.dart';
+import 'package:t_store/utils/constants/sizes.dart';
+import 'package:t_store/utils/constants/text_strings.dart';
+
+import '../../../../common/widgets/login_signup/form_divider.dart';
+import '../../../../common/widgets/login_signup/social_buttons.dart';
+
+class SignupScreen extends StatelessWidget {
+  const SignupScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: SingleChildScrollView(
+          child: Padding(
+        padding: TSpacingStyleSignupScreen.paddingWithAppBarHeight,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            // title
+            Text(
+              TTexts.signupTitle,
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+            const SizedBox(
+              height: TSizes.spaceBtwSections,
+            ),
+
+            // form
+            const TSignupForm(),
+
+            // divider
+            const TFormDivider(dividerText: TTexts.orSignUpWith),
+            const SizedBox(
+              height: TSizes.spaceBtwSections / 4,
+            ),
+
+            // social media button
+            const TSocialButtons()
+          ],
+        ),
+      )),
+    );
+  }
+}
